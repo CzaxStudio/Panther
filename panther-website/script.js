d997cf54e8720e001b4a622e9b125916ab313d82
Hacker <minubgp@gmail.com>
2026-02-21 11:13:43 +0530

    Update script.js

--- panther-website/script.js
+++ panther-website/script.js
@@ -1,22 +1,56 @@
-const button = document.getElementById("downloadBtn");
+document.addEventListener("DOMContentLoaded", function () {
 
-const userOS = navigator.userAgent;
+    const downloadBtn = document.getElementById("downloadBtn");
+    const osText = document.getElementById("osText");
 
-if (userOS.includes("Win")) {
-    button.innerText = "Download for Windows";
-    button.onclick = () => {
-        window.location.href = "https://yourdomain.com/downloads/panther.exe";
-    };
+    const windowsLink = "https://github.com/CzaxStudio/Panther/releases/download/Windows/Panther.exe";
+    const linuxLink   = "https://github.com/CzaxStudio/Panther/releases/download/Linux/Panther";
 
-} else if (userOS.includes("Linux")) {
-    button.innerText = "Download for Linux";
-    button.onclick = () => {
-        window.location.href = "https://yourdomain.com/downloads/panther";
-    };
+    const userAgent = navigator.userAgent.toLowerCase();
 
-} else {
-    button.innerText = "Download for Linux";
-    button.onclick = () => {
-        window.location.href = "https://yourdomain.com/downloads/panther";
-    };
-}
+    function detectOS() {
+        if (userAgent.includes("win")) {
+            return "Windows";
+        } 
+        else if (userAgent.includes("linux")) {
+            return "Linux";
+        } 
+        else if (userAgent.includes("mac")) {
+            return "Mac";
+        } 
+        else {
+            return "Unknown";
+        }
+    }
+
+    const os = detectOS();
+
+    // Update UI based on OS
+    if (os === "Windows") {
+        downloadBtn.innerText = "Download for Windows";
+        osText.innerText = "Detected OS: Windows";
+        downloadBtn.onclick = () => {
+            window.location.href = windowsLink;
+        };
+    }
+    else if (os === "Linux") {
+        downloadBtn.innerText = "Download for Linux";
+        osText.innerText = "Detected OS: Linux";
+        downloadBtn.onclick = () => {
+            window.location.href = linuxLink;
+        };
+    }
+    else if (os === "Mac") {
+        downloadBtn.innerText = "Mac Not Supported";
+        osText.innerText = "Detected OS: macOS (Currently Not Supported)";
+        downloadBtn.disabled = true;
+    }
+    else {
+        downloadBtn.innerText = "Download Panther";
+        osText.innerText = "Could not detect OS. Please choose manually.";
+        downloadBtn.onclick = () => {
+            window.location.href = "https://github.com/CzaxStudio/Panther/releases";
+        };
+    }
+
+});
